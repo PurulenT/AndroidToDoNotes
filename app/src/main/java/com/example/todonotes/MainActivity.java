08e558eb8884e73aead6d2a43114f5e7e0e9553f
Норберт Ягмуров <nuri.handsome45@gmail.com>
2026-08-11 17:54:19 +0300

    Created a project with a floating add button in the layout and a full-screen scrolling view, which currently contains a simple linear list, which will be replaced with a recycler in the future. We created a note class with text, ID, and priority, and also created a mockup of how a single note will look on the screen. We instantiated elements in code, initialized the button and the linear list, created 10 random instances of the note class, and inflated them using the layout

---
+++ app/src/main/java/com/example/todonotes/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.todonotes;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayoutNotes;
+    private FloatingActionButton btnAddNote;
+
+    private ArrayList<Note> notes = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        initViews();
+        Random random = new Random();
+        for(int i = 0; i < 10; i++){
+            notes.add(new Note(1, "text content" + i, random.nextInt(3)));
+        }
+        showNotes();
+    }
+
+    private void initViews(){
+        btnAddNote = findViewById(R.id.btnAddNote);
+        linearLayoutNotes = findViewById(R.id.linearLayoutNotes);
+    }
+
+    private void showNotes(){
+        for (Note note : notes){
+            View view = getLayoutInflater().inflate(R.layout.note_item, linearLayoutNotes, false);
+            TextView textViewNote = view.findViewById(R.id.textViewNote);
+            textViewNote.setText(note.getText());
+
+            int colorResId;
+            switch(note.getPriority()){
+                case 0:
+                    colorResId = android.R.color.holo_green_light;
+                    break;
+                case 1:
+                    colorResId = android.R.color.holo_orange_light;
+                    break;
+                default:
+                    colorResId = android.R.color.holo_red_light;
+                    break;
+            }
+            int color = ContextCompat.getColor(this, colorResId);
+            textViewNote.setBackgroundColor(color);
+
+            linearLayoutNotes.addView(view);
+        }
+    }
+}
