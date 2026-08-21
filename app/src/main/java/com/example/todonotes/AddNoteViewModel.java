bca18f834f0a528e78922ca0e1af17994c33c424
Норберт Ягмуров <nuri.handsome45@gmail.com>
2026-08-21 23:44:42 +0300

    started using disposable and composite disposable objects. These classes allow to cancel all the processes when screen (activity) closed before they finished. So now all the processes inside view model are strictly related to the activity lifecycle. If the process has delay time and user quits screen under this time - the process will be aborted

--- app/src/main/java/com/example/todonotes/AddNoteViewModel.java
+++ app/src/main/java/com/example/todonotes/AddNoteViewModel.java
@@ -8,13 +8,15 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class AddNoteViewModel extends AndroidViewModel {
     NoteDatabase noteDatabase;
     MutableLiveData<Boolean> shouldClose = new MutableLiveData<>(false);
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     public AddNoteViewModel(@NonNull Application application) {
         super(application);
@@ -22,7 +24,7 @@ public class AddNoteViewModel extends AndroidViewModel {
     }
 
     public void saveNote(Note note){
-        noteDatabase.notesDao().add(note)
+        Disposable disposable = noteDatabase.notesDao().add(note)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action() {
@@ -31,9 +33,16 @@ public class AddNoteViewModel extends AndroidViewModel {
                         shouldClose.setValue(true);
                     }
                 });
+        compositeDisposable.add(disposable);
     }
 
     public LiveData<Boolean> getShouldClose(){
         return shouldClose;
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.dispose();
     }
 }
